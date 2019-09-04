7fe97b142587f6f64a23d733ca7a0127cb3e50f0
吴晓林2 <wuxl@weipaitang.com>
2019-09-04 14:21:16 +0800

    测试composer包

---
+++ Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuxiaolin
+ * Date: 2019-09-04
+ * Time: 14:17
+ */
+require_once './vendor/autoload.php';
+use Demo\Abc\Demo;
+
+$demo = new Demo();
+$demo->index();
---
+++ src/Demo/Abc/Demo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuxiaolin
+ * Date: 2019-09-04
+ * Time: 14:11
+ */
+namespace Demo\Abc;
+class Demo
+{
+    public function index()
+    {
+        echo "hello word\n";
+    }
+}
